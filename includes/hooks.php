4c94587f4e89d518ad39d0dac20f36bd6273438a
flowdee <ddjjmm@gmx.de>
2017-11-06 10:28:57 +0100

    register our custom image sizes with "init" not "admin_init"

--- includes/hooks.php
+++ includes/hooks.php
@@ -18,7 +18,7 @@ function affcoups_add_image_sizes() {
     add_image_size( 'affcoups-thumb', 480, 250, array( 'center', 'top' ) );
     add_image_size( 'affcoups-thumb-small', 144, 75, array( 'center', 'top' ) );
 }
-add_action( 'admin_init', 'affcoups_add_image_sizes' );
+add_action( 'init', 'affcoups_add_image_sizes' );
 
 /**
  * Custom CSS
