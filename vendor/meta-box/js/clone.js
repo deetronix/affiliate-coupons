a0481913cf5a976401b59322befeb2c450ec877e
flowdee <ddjjmm@gmx.de>
2017-11-20 17:49:58 +0100

    updated meta box dependency to latest v4.12.5 resolves #57

--- vendor/meta-box/js/clone.js
+++ vendor/meta-box/js/clone.js
@@ -92,6 +92,7 @@ jQuery( function ( $ ) {
 		reset: function() {
 			cloneValue.$field = $( this );
 			cloneValue.type = cloneValue.$field.attr( 'type' );
+			cloneValue.isHiddenField = cloneValue.$field.hasClass( 'rwmb-hidden' );
 
 			if ( true === cloneValue.$field.data( 'clone-default' ) ) {
 				cloneValue.resetToDefault();
@@ -110,7 +111,7 @@ jQuery( function ( $ ) {
 				cloneValue.$field.prop( 'checked', !!defaultValue );
 			} else if ( 'select' === cloneValue.type ) {
 				cloneValue.$field.find( 'option[value="' + defaultValue + '"]' ).prop( 'selected', true );
-			} else if ( 'hidden' !== cloneValue.type ) {
+			} else if ( ! cloneValue.isHiddenField ) {
 				cloneValue.$field.val( defaultValue );
 			}
 		},
@@ -122,7 +123,7 @@ jQuery( function ( $ ) {
 				cloneValue.$field.prop( 'checked', false );
 			} else if ( 'select' === cloneValue.type ) {
 				cloneValue.$field.prop( 'selectedIndex', - 1 );
-			} else if ( 'hidden' !== cloneValue.type ) {
+			} else if ( ! cloneValue.isHiddenField ) {
 				cloneValue.$field.val( '' );
 			}
 		}
@@ -221,7 +222,7 @@ jQuery( function ( $ ) {
 			.data( 'next-index', $container.children( '.rwmb-clone' ).length )
 			.sortable( {
 				handle: '.rwmb-clone-icon',
-				placeholder: ' rwmb-clone rwmb-clone-placeholder',
+				placeholder: ' rwmb-clone rwmb-sortable-placeholder',
 				items: '.rwmb-clone',
 				start: function ( event, ui ) {
 					// Make the placeholder has the same height as dragged item
