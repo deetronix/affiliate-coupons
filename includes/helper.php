09ae5c960435db7171d6ec45a3e010710ba9549b
flowdee <ddjjmm@gmx.de>
2018-04-04 18:45:18 +0200

    refactored helper.php according to coding standards

--- includes/helper.php
+++ includes/helper.php
@@ -6,43 +6,45 @@
  * @since       1.0.0
  */
 
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 /**
  * Cleanup string in order to be used inside html attributes (title, alt etc.) without breaking the markup
  *
  * @param $str
- * @return mixed
+ *
+ * @return mixed|string
  */
 function affcoups_cleanup_html_attribute( $str ) {
 
-    // Remove html
-    $str = strip_tags( $str );
+	// Remove html.
+	$str = strip_tags( $str );
 
-    // Remove apostrophes
-    $str = str_replace( array( '"', "'" ), '', $str );
+	// Remove apostrophes.
+	$str = str_replace( array( '"', "'" ), '', $str );
 
-    return $str;
+	return $str;
 }
 
 /**
  * Convert separated strings to array
  *
  * @param $string
+ *
  * @return array
  */
 function affcoups_convert_separated_strings_to_array( $string ) {
 
-    // Remove spaces
-    $string = str_replace( ' ', '', $string);
+	// Remove spaces.
+	$string = str_replace( ' ', '', $string );
 
-    // Convert to array
-    $array = explode( ',', $string );
+	// Convert to array.
+	$array = explode( ',', $string );
 
-    return $array;
+	return $array;
 }
 
 /**
@@ -51,7 +53,7 @@ function affcoups_convert_separated_strings_to_array( $string ) {
  * return array options or empty when not available
  */
 function affcoups_get_options() {
-    return get_option( 'affcoups_settings', array() );
+	return get_option( 'affcoups_settings', array() );
 }
 
 /**
@@ -59,19 +61,20 @@ function affcoups_get_options() {
  *
  * @param $key
  * @param null $default
+ *
  * @return null
  */
 function affcoups_get_option( $key, $default = null ) {
-    $options = affcoups_get_options();
+	$options = affcoups_get_options();
 
-    return ( isset( $options[$key] ) ) ? $options[$key] : $default;
+	return ( isset( $options[ $key ] ) ) ? $options[ $key ] : $default;
 }
 
 /**
  * Output public assets folder url
  */
 function affcoups_the_assets() {
-    echo affcoups_get_assets_url();
+	echo esc_url( affcoups_get_assets_url() );
 }
 
 /**
@@ -80,14 +83,14 @@ function affcoups_the_assets() {
  * @return string
  */
 function affcoups_get_assets_url() {
-    return AFFCOUPS_URL . 'public/';
+	return AFFCOUPS_URL . 'public/';
 }
 
 /**
  * Check whether it's development environment or not
  */
 function affcoups_is_development() {
-    return ( strpos( get_bloginfo('url'), 'affcoups.dev' ) !== false ) ? true : false;
+	return ( strpos( get_bloginfo( 'url' ), 'affcoups.dev' ) !== false ) ? true : false;
 }
 
 /**
@@ -95,13 +98,15 @@ function affcoups_is_development() {
  */
 function affcoups_is_amp() {
 
-    if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() )
-        return true;
+	if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
+		return true;
+	}
 
-    if ( function_exists( 'is_wp_amp' ) && is_wp_amp() )
-        return true;
+	if ( function_exists( 'is_wp_amp' ) && is_wp_amp() ) {
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -111,9 +116,9 @@ function affcoups_is_amp() {
  */
 function affcoups_is_plugin_admin_area() {
 
-    $screen = get_current_screen();
+	$screen = get_current_screen();
 
-    return ( strpos( $screen->id, 'affcoups') !== false ) ? true : false;
+	return ( strpos( $screen->id, 'affcoups' ) !== false ) ? true : false;
 }
 
 /**
@@ -121,21 +126,22 @@ function affcoups_is_plugin_admin_area() {
  *
  * @param $path
  * @param string $source
+ *
  * @return string
  */
 function affcoups_get_website_url( $path = '', $source = 'plugin-settings' ) {
 
-    $url = 'https://affcoups.com/';
+	$url = 'https://affcoups.com/';
 
-    if ( ! empty( $path ) )
-        $url .= trim( $path,'/') . '/';
+	if ( ! empty( $path ) ) {
+		$url .= trim( $path, '/' ) . '/';
+	}
 
-    return esc_url( add_query_arg( array(
-            'utm_source'   => $source,
-            'utm_medium'   => 'plugin-row',
-            'utm_campaign' => 'Affiliate Coupons',
-        ), $url )
-    );
+	return esc_url( add_query_arg( array(
+		'utm_source'   => $source,
+		'utm_medium'   => 'plugin-row',
+		'utm_campaign' => 'Affiliate Coupons',
+	), $url ) );
 }
 
 /**
@@ -144,25 +150,27 @@ function affcoups_get_website_url( $path = '', $source = 'plugin-settings' ) {
  * @param $str
  * @param int $limit
  * @param string $pad
+ *
  * @return mixed|string
  */
 function affcoups_truncate_string( $str, $limit = 200, $pad = '...' ) {
 
-    $limit = intval( $limit );
+	$limit = intval( $limit );
 
-    if ( strlen( $str ) > $limit ) {
-        $str = preg_replace('/\s+?(\S+)?$/', '', substr( $str, 0, $limit + 1 ) );
+	if ( strlen( $str ) > $limit ) {
+		$str = preg_replace( '/\s+?(\S+)?$/', '', substr( $str, 0, $limit + 1 ) );
 
-        $str = rtrim( $str, '.' );
-        $str = rtrim( $str, ',' );
-        $str = rtrim( $str, '-' );
+		$str = rtrim( $str, '.' );
+		$str = rtrim( $str, ',' );
+		$str = rtrim( $str, '-' );
 
-        $str .= $pad;
-    }
+		$str .= $pad;
+	}
 
-    return $str;
+	return $str;
 }
 
+// @codingStandardsIgnoreStart
 /**
  * Debug
  *
@@ -171,15 +179,15 @@ function affcoups_truncate_string( $str, $limit = 200, $pad = '...' ) {
  */
 function affcoups_debug( $args, $title = false ) {
 
-    if ( $title ) {
-        echo '<h3>' . $title . '</h3>';
-    }
+	if ( $title ) {
+		echo '<h3>' . esc_html( $title ) . '</h3>';
+	}
 
-    if ( $args ) {
-        echo '<pre>';
-        print_r($args);
-        echo '</pre>';
-    }
+	if ( $args ) {
+		echo '<pre>';
+		print_r( $args );
+		echo '</pre>';
+	}
 }
 
 /**
@@ -187,14 +195,16 @@ function affcoups_debug( $args, $title = false ) {
  *
  * @param $log
  */
-function affcoups_debug_log ( $log )  {
+function affcoups_debug_log( $log ) {
 
-    if ( ! affcoups_is_development() )
-        return;
+	if ( ! affcoups_is_development() ) {
+		return;
+	}
 
-    if ( is_array( $log ) || is_object( $log ) ) {
-        error_log( print_r( $log, true ) );
-    } else {
-        error_log( $log );
-    }
+	if ( is_array( $log ) || is_object( $log ) ) {
+		error_log( print_r( $log, true ) );
+	} else {
+		error_log( $log );
+	}
 }
+// @codingStandardsIgnoreEnd
