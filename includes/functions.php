9185893f2295f16913af58bc9e6840856e7a90ea
flowdee <mail@flowdee.de>
2018-11-12 16:52:23 +0100

    reordered orderby options

--- includes/functions.php
+++ includes/functions.php
@@ -448,14 +448,14 @@ function affcoups_get_style_options() {
 function affcoups_get_orderby_options() {
 
     $options = array(
+        'name'        => __( 'Name (Post)', 'affiliate-coupons' ),
+        'date'        => __( 'Date published (Post)', 'affiliate-coupons' ),
         'title'       => __( 'Title (Coupon)', 'affiliate-coupons' ),
         'description' => __( 'Description (Coupon)', 'affiliate-coupons' ),
         'discount'    => __( 'Discount (Coupon)', 'affiliate-coupons' ),
         'valid_from'  => __( 'Valid from date (Coupon)', 'affiliate-coupons' ),
         'valid_to'    => __( 'Valid to date (Coupon)', 'affiliate-coupons' ),
-        'random'      => __( 'Random', 'affiliate-coupons' ),
-        'name'        => __( 'Name (Post)', 'affiliate-coupons' ),
-        'date'        => __( 'Date published (Post)', 'affiliate-coupons' ),
+        'random'      => __( 'Random', 'affiliate-coupons' )
     );
 
     $options = apply_filters( 'affcoups_orderby_options', $options );
