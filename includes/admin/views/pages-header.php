e8afefcb9a6ef2bf47fa587c5bf4123c89b12e16
flowdee <mail@flowdee.de>
2021-01-12 09:34:35 +0100

    updated admin pages header

--- includes/admin/views/pages-header.php
+++ includes/admin/views/pages-header.php
@@ -18,12 +18,14 @@ $is_pro = affcoups_is_pro_version();
         <a
             href="<?php echo ( $is_pro ) ? 'https://affcoups.com/account/' : 'https://affcoups.com/features/'; ?>"
             target="_blank"
-        ><i class='dashicons dashicons-text-page'></i><?php echo ( $is_pro ) ? __( 'Your AffCoups Account', 'affiliate-coupons' ) : __( 'Upgrade to the Pro Version', 'affiliate-coupons' ); ?></a>
+        ><i class='dashicons dashicons-text-page'></i><?php echo ( $is_pro ) ? __( 'Your Account', 'affiliate-coupons' ) : __( 'Upgrade to the Pro Version', 'affiliate-coupons' ); ?></a>
 
-        <a href="https://affcoups.com/docs/" target="_blank"><i class='dashicons dashicons-text-page'></i><?php _e( 'Documentation', 'affiliate-coupons' ); ?></a>
+        <a href="https://affcoups.com/docs/" target="_blank" rel="nofollow"><i class='dashicons dashicons-text-page'></i><?php _e( 'Documentation', 'affiliate-coupons' ); ?></a>
     </div>
 
+    <?php /*
     <div id="affcoups-admin-header-notice">
         <?php _e( 'Looking to speed up WordPress? Check out our <a href="https://woorkup.com/speed-up-wordpress/?utm_source=perfmatters&utm_medium=banner&utm_campaign=header-cta" title="WordPress Optimization Guide" target="_blank">complete optimization guide</a>.', 'affiliate-coupons' ); ?>
     </div>
+    <?php */ ?>
 </div>
